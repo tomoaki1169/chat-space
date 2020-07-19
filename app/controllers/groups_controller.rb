432cf2f8db877a59b8b8d580ba3f3af129e98b45
tomoaki1169 <0111tomoaki@gmail.com>
2020-07-19 20:59:00 +0900

    インクリメント2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to group_messsages_path(@group),notice: 'グループを更新しました'
+      redirect_to  group_messages_path(@group),notice: 'グループを更新しました'
     else
       render :edit
     end
