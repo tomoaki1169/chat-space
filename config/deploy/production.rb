4e19a8dd7a6c01670f2c0c1a43f6837b3713ae55
tomoaki1169 <0111tomoaki@gmail.com>
2020-07-21 21:07:54 +0900

    deploy.new

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,5 +59,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '13.114.253.38/', user: 'ec2-user', roles: %w{app db web}
+server '13.114.253.38', user: 'ec2-user', roles: %w{app db web}
 
